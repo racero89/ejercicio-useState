39e8778e770c948f3877f228a680a383ff22abf0
Monica <monica.racero@gmail.com>
2024-06-22 11:05:00 +0200

    monica

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,23 @@ import './App.css'
 
 function App() {
  //Aquí tu código
+const[name, setName]= useState("Sofia");
+const[newName,setNewName]= useState("");
+
+
+return(
+    <div>
+<h1>Nombre de profesor:{name}</h1>
+<input
+type='text'
+value={newName}
+onChange={(e)=> setNewName(e,target)}
+placeholder="Ingresa el nuevo nombre"
+/>
+<button onClick={() => setName(newName)}>Actualizar Nombre</button>
+    </div>
+)
+
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
+function teacheName(){
+  const[name, setName]= useState("Sofia");
+  const[newName, setNewName]= useState("");
+  const predefinedNames = ['Data', 'Reyes', 'Yolanda'];
+  
+  return(
+    <div>
+      <h2>Teacher Name: {name}</h2>
+      type="text" 
+        value={newName} 
+        onChange={(e) => setNewName(e.target.value)} 
+        placeholder="Ingresa el nuevo nombre"
+      
+      <button onClick={() => setName(newName)}>Actualizar Nombre</button>
+      <ul>
+        {predefinedNames.map((predefinedName, index) => (
+          <li key={index} onClick={() => setName(predefinedName)}>
+            {predefinedName}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+export default teacheName;
